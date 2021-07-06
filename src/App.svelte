2b29cb1a8158b894aed3442f96be3bb90ad80e85
Luis Bonilla <blessedluismi@gmail.com>
2021-07-05 22:30:59 -0400

    added footer
    - Footer component (light version)
    - icons corresponding the footer.
    - Missing links for each action button.

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,33 @@
 <script>
-	import Cta from './components/CTA.svelte';
-	import Hero from './components/Hero.svelte';
-	import HomeSection from './components/HomeSection.svelte';
-	import Navbar from './components/Navbar.svelte';
-	import ScrollingTitle from './components/ScrollingTitle.svelte';
-	import { ourwork } from './OurWorkContent';
+  import Cta from "./components/CTA.svelte";
+  import Hero from "./components/Hero.svelte";
+  import HomeSection from "./components/HomeSection.svelte";
+  import Navbar from "./components/Navbar.svelte";
+  import ScrollingTitle from "./components/ScrollingTitle.svelte";
+  import { ourwork } from "./OurWorkContent";
+  import Footer from "./components/Footer.svelte";
 </script>
 
 <main class="container">
-	<Navbar />
-	<Hero />
-	<Cta />
-	<ScrollingTitle />
-	<HomeSection title="our work" content={ourwork} />
+  <Navbar />
+  <Hero />
+  <Cta />
+  <ScrollingTitle />
+  <HomeSection title="our work" content={ourwork} />
+  <Footer />
 </main>
 
 <style global>
-	@tailwind base;
-	@tailwind components;
-	@tailwind utilities;
-	body {
-		@apply font-mono bg-light antialiased;
-	}
-	::selection {
-		@apply text-dark  bg-fire-1 bg-opacity-60;
-	}
-	main.container {
-		@apply w-full;
-	}
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+  body {
+    @apply font-mono bg-light antialiased;
+  }
+  ::selection {
+    @apply text-dark  bg-fire-1 bg-opacity-60;
+  }
+  main.container {
+    @apply w-full;
+  }
 </style>
---
+++ src/components/Footer.svelte
@@ -0,0 +1,44 @@
+<div class="flex items-center justify-between px-5 pb-2 bg-deepGrey pt-7">
+  <img src="/assets/FireLogoAlone.svg" alt="The DevFire Labs logo" class="" />
+</div>
+<div class="flex items-start justify-start px-5 pt-6 pb-4 bg-deepGrey">
+  <button class="mr-8"
+    ><img
+      src="/assets/iglogowhite.svg"
+      alt="DevFire Labs Instagram"
+      class=""
+    /></button
+  >
+  <button class="mx-8"
+    ><img
+      src="/assets/twitterlogoWhite.svg"
+      alt="DevFire Labs Twitter"
+      class=""
+    /></button
+  >
+  <button class="mx-8"
+    ><img
+      src="/assets/youtubelogowhite.svg"
+      alt="DevFire Labs Youtube channel"
+      class=""
+    /></button
+  >
+</div>
+
+<div class="flex flex-col items-start px-5 pt-2 pb-4 bg-deepGrey">
+  <h1 href="" class="text-2xl text-white font-display">Company</h1>
+  <a href="" class="pt-2 font-sans font-light text-white text-md">About Us</a>
+  <a href="" class="pt-2 font-sans font-light text-white text-md"
+    >Contact & Support</a
+  >
+  <h1 href="" class="pt-4 text-2xl text-white font-display">Services</h1>
+  <a href="" class="pt-2 font-sans font-light text-white text-md">Our Work</a>
+  <a href="" class="pt-2 font-sans font-light text-white text-md">Services</a>
+
+  <a href="" class="pt-6 font-sans font-light text-white text-md">
+    Cookie & Privacy Policy
+  </a>
+  <p class="pt-2 font-sans text-md text-fire-1">
+    © 2021 DevFire Labs All rights reserved.
+  </p>
+</div>
