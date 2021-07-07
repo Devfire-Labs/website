18077c5278de23ea619f04742fd54db53a708bdb
Raul Luis <soyrauluis@outlook.com>
2021-07-07 18:25:09 -0400

    Make components for the rest of planned pages

---
+++ src/pages/HitUsUp.svelte
@@ -0,0 +1,23 @@
+<div class="hit-us-up">
+	<h1 class="">Hit Us Up</h1>
+	<p class="">
+		Don’t want to fill out forms? You can email us at:
+		<strong>info@devfirelabs.com</strong>
+	</p>
+	<div class="bg-fire-1">
+		<label for="" class="label"
+			>Name<input type="text" class="text-input" /></label
+		>
+		<label for="" class="label"
+			>Email<input type="text" class="text-input" /></label
+		>
+		<label for="" class="label"
+			>Tell us about your project<textarea class="textarea-input" /></label
+		>
+		<label for="" class="checkbox-label"
+			><input type="checkbox" />I hereby accept DevFire Labs General Terms and
+			Privacy Policy.</label
+		>
+		<button type="submit">Send message</button>
+	</div>
+</div>
