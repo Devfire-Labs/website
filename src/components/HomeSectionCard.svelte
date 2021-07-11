8060a5de470a568df331f0ccb5c221aa89b459cc
Luis Bonilla <blessedluismi@gmail.com>
2021-07-10 20:20:17 -0400

    working on the Home Section for Tablet Size

--- src/components/HomeSectionCard.svelte
+++ src/components/HomeSectionCard.svelte
@@ -1,20 +1,36 @@
 <script>
-	import { Link } from 'yrv';
+  import { Link } from "yrv";
 
-	export let post, id;
-	const { title, body, img } = post;
+  export let post, id;
+  const { title, body, img } = post;
 </script>
 
 <div class="project-card">
-	<Link href={`/blog/${id}`}>
-		<img src={img.src} alt={img.alt} class="" />
-		<h3 class=" font-display text-2xl mt-2">{title}</h3>
-		<p class=" font-mono text-lg mt-1.5">{@html body}</p>
-	</Link>
+  <Link href={`/blog/${id}`}>
+    <img src={img.src} alt={img.alt} class="" />
+
+    <div class="md:relative md:-mt-36 ">
+      <div class="md:p-6 ">
+        <h3
+          class="mt-2 text-2xl md:text-6xl md:text-white md:absolute md:wrapper font-display"
+        >
+          {title}
+        </h3>
+        <p
+          class="md:text-2xl md:text-white md:relative font-mono text-lg mt-1.5 md:mt-20"
+        />
+      </div>
+    </div>
+    {@html body}
+  </Link>
 </div>
 
 <style>
-	.project-card {
-		@apply mt-5;
-	}
+  .project-card {
+    @apply mt-5;
+  }
+
+  /* .text-position {
+    @apply md:mb-16;
+  } */
 </style>
