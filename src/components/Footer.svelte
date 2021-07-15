cb8f6be596f1bb66b7d6f702cf3a6e9a9333b104
Luis Bonilla <blessedluismi@gmail.com>
2021-07-14 23:31:00 -0400

    LG footer size modified. Missing XL and 2XL

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -5,7 +5,7 @@
 
 <div class="footer md:flex md:justify-between md:align-top">
   <!-- newsletter section -->
-  <div class="hidden md:mt-4 md:block">
+  <div class="hidden md:block lg:px-6 lg:mt-0 lg:mb-4">
     <h1 class="inline-block pb-5 text-2xl md:pb-3 font-display md:px-0">
       Newsletter
     </h1>
@@ -18,7 +18,7 @@
     >
   </div>
 
-  <!-- Logo -->
+  <!-- Logo SM -->
   <img
     src="/assets/FireLogoAlone.svg"
     alt="The DevFire Labs logo"
@@ -32,19 +32,20 @@
   </div>
 
   <!-- company -->
-  <div class="flex flex-col items-start pt-2 md:pt-0 md:mr-30">
+  <div class="flex flex-col items-start pt-2 md:pt-0 md:mr-30 lg:mr-12">
     <h1 class="subtitle">Company</h1>
     <Link href="/about" class="footer-link">About Us</Link>
     <Link href="" class="footer-link">Contact & Support</Link>
   </div>
 
   <!-- services -->
-  <div class="flex flex-col items-start pt-2 md:mr-15 md:pt-0">
+  <div class="flex flex-col items-start pt-2 md:mr-15 md:pt-0 lg:mr-8">
     <h1 class="subtitle">Services</h1>
     <Link href="" class="footer-link">Services</Link>
     <Link href="" class="footer-link">Portfolio</Link>
   </div>
 
+  <!-- social icons for MD,LG,Xl -->
   <div class="socials">
     <h1 class="pb-5 text-2xl font-display ">Follow us:</h1>
     <SocialIconsLight />
@@ -62,7 +63,7 @@
   <img
     src="/assets/logolightwhite.svg"
     alt="The DevFire Labs logo"
-    class="items-start align-middle md:m-3 md:px-0"
+    class="items-start align-middle md:m-3 md:px-0 lg:px-5"
   />
 
   <a href="" class="cookies"> Cookie & Privacy Policy </a>
@@ -71,7 +72,7 @@
 
 <style>
   .cookies {
-    @apply font-sans text-sm font-light text-white md:mt-7 md:ml-20;
+    @apply font-sans text-sm lg:text-lg lg:ml-36 font-light text-white md:mt-7 lg:mt-5 md:ml-20;
   }
 
   .hiddenBox {
@@ -79,11 +80,11 @@
   }
 
   .socials {
-    @apply hidden mt-9 md:mt-4 md:pr-10 md:flex-col md:flex md:items-center;
+    @apply hidden mt-9 md:mt-4 md:pr-10 md:flex-col lg:ml-0 lg:mr-6 md:flex md:items-center;
   }
 
   .rights {
-    @apply font-sans text-sm font-light text-white md:mt-7 md:pr-4 md:-ml-12;
+    @apply font-sans text-sm lg:text-lg lg:mt-5 font-light text-white md:mt-7 md:pr-4 md:-ml-12;
   }
 
   .footer {
