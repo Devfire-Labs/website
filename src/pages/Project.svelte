7297b14641978aeabd12efc4bed14039d4cb8a4e
Luis Bonilla <blessedluismi@gmail.com>
2021-07-19 00:07:52 -0400

    updates on the Project section

--- src/pages/Project.svelte
+++ src/pages/Project.svelte
@@ -6,17 +6,19 @@
   console.log("Post: ", post);
 </script>
 
-<div class="container mb-12 mr-2 mt-7 md:ml-2 md:mr-4">
+<div class="container mb-12 mr-2 mt-7 md:ml-2 md:mr-4 xl:ml-14">
   <div class="px-4">
-    <h1 class="mt-10 text-3xl font-display md:text-4xl">{title}</h1>
-    <h2 class="mt-1 mb-3 font-mono text-xl font-bold md:text-2xl">{type}</h2>
+    <h1 class="mt-10 text-3xl font-display md:text-4xl xl:text-6xl">{title}</h1>
+    <h2 class="mt-1 mb-3 font-mono text-xl font-bold md:text-2xl xl:text-3xl">
+      {type}
+    </h2>
   </div>
   <div class="scrolling-wrapper">
     {#each imgCarousel as { src, alt }, i (i)}
       <img {src} {alt} class=" thumbnail" />
     {/each}
   </div>
-  <p class="px-5 my-5 font-mono md:my-7">{@html body}</p>
+  <p class="px-5 my-5 font-mono md:my-7 xl:my-8 xl:text-xl">{@html body}</p>
 </div>
 
 <style>
