67acf4306d800ff9d0f427cba694f3838f734771
Raul Luis <soyrauluis@outlook.com>
2021-07-19 22:02:50 -0400

    Converted Hero text to a svg

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,22 +1,10 @@
 <section class="mt-12 lg:px-20 2xl:px-0">
 	<div class="md:flex-row md:flex">
-		<h1
-			class="relative pl-6 text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl font-display"
-		>
-			Reliable software solutions, <br /> a
-			<u>click</u>
-			<!-- <img
-				src="/assets/click.svg"
-				alt="A pointer"
-				class="absolute filter drop-shadow-lg bottom-12 left-44 lg:bottom-32 lg:left-48 xl:bottom-48 xl:left-60 xl:w-20 2xl:left-64 2xl:bottom-32"
-			/>
-			  --><br
-			/> away.
-		</h1>
+		<img src="/assets/hero-text.svg" alt="" class="pl-6 pr-4 w-full md:w-4/5" />
 		<img
 			src="/assets/hero-img.jpg"
 			alt=""
-			class="object-contain w-5/6 mx-auto mt-6 md:mt-0 md:w-3/5 md:object-cover md:mx-auto md:h-3/4 md:pr-6 md:mb-4"
+			class="object-contain w-5/6 mx-auto mt-6 md:mt-0 md:w-3/5 md:object-cover md:h-3/4 md:pr-6 md:mb-4"
 		/>
 	</div>
 </section>
