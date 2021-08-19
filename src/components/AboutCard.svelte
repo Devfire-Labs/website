7f18eb97e3d3b56e41f0b9495e9489fe32043b50
Luis Bonilla <blessedluismi@gmail.com>
2021-08-18 20:58:21 -0400

    About Us
    Working on the About Us section.

--- src/components/AboutCard.svelte
+++ src/components/AboutCard.svelte
@@ -1,38 +1,42 @@
 <script>
-	import { darkMode } from '../stores/darkMode';
-	export let title, img, description;
-	const { src, alt } = img;
+  import { darkMode } from "../stores/darkMode";
+  export let title, img, description;
+  const { src, alt } = img;
 </script>
 
 <div
-	class="flex m-4 mt-6"
-	class:bg-white={!$darkMode}
-	class:bg-onix={$darkMode}
+  class="flex m-4 mt-6"
+  class:bg-white={!$darkMode}
+  class:bg-onix={$darkMode}
 >
-	<div class="flex-grow px-5" class:bg-fire-1={!$darkMode} class:bg-fire-2={$darkMode} />
-	<div class="container">
-		<div class="textContainer">
-			<h1 class="titleText">{title}</h1>
-			<img {src} {alt} class="w-8 h-8" />
-		</div>
-		<p class="description">{description}</p>
-	</div>
+  <div
+    class="flex-grow px-5"
+    class:bg-fire-1={!$darkMode}
+    class:bg-fire-2={$darkMode}
+  />
+  <div class="container">
+    <div class="textContainer">
+      <h1 class="titleText">{title}</h1>
+      <img {src} {alt} class="w-8 h-8" />
+    </div>
+    <p class="description">{description}</p>
+  </div>
 </div>
 
 <style>
-	.container {
-		@apply flex flex-col w-full px-4 py-4;
-	}
+  .container {
+    @apply flex flex-col w-full px-4 py-4 whitespace-pre-line;
+  }
 
-	.textContainer {
-		@apply flex justify-between w-full;
-	}
+  .textContainer {
+    @apply flex justify-between w-full;
+  }
 
-	.titleText {
-		@apply text-3xl font-display;
-	}
+  .titleText {
+    @apply text-3xl font-display;
+  }
 
-	:global(.description) {
-		@apply pt-2 pr-4 font-sans text-lg;
-	}
+  :global(.description) {
+    @apply pt-2 pr-4 font-sans text-lg;
+  }
 </style>
