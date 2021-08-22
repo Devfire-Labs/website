85eac21ef304798fc7c663f00f5784c7ae44683a
Raul Luis <soyrauluis@outlook.com>
2021-08-22 17:46:44 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 import preprocess from 'svelte-preprocess';
+import adapterNetlify from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: adapterNetlify(),
 	},
 
 	preprocess: [
